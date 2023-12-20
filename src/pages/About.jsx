1f1e999b58bd5029a4b3377404e87f7f03232450
Travis <travis3214@yahoo.com>
2023-12-20 16:36:58 -0500

    set up the basic layout for about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,15 @@
 export default function About() {
     return (
-      <div>
-        <h1 className="title">About me</h1>
+      <div className="title">
+        <h1>About me</h1>
+        <div className="aboutPage">
+          <p>A recent graduate from the University of Central Florida. My degree was in Health Sciences (Pre-Clinical track) </p>
+          <img className="me" src="/me.jpeg" alt="" />
+        </div>
+        <div className="aboutPage">
+          <img className="me" src="/me.jpeg" alt="" />
+          <p>hey</p>
+        </div>
       </div>
     );
   }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import { ContactForm } from "../components/ContactForm";
 export default function Contact() {
     return (
       <div className="title">
-        <h1 className="">Contact Me</h1>
+        <h1>Contact Me</h1>
         <ContactForm />
       </div>
     );
