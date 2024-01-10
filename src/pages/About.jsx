ea1ceed75f44c69f8030a4713ef5f364f8c0cdac
Travis <travis3214@yahoo.com>
2024-01-09 22:19:52 -0500

    updated about page with pic

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,21 @@
 export default function About() {
     return (
       <div className="title">
-        <h1>About me</h1>
         <div className="aboutPage">
-          <p className="aboutText">A recent graduate from the University of Central Florida. My degree was in Health Sciences (Pre-Clinical track) but i quickily realized my passion was always technology</p>
-          <img className="me" src="/me.jpeg" alt="" />
+          <div>
+            <h1 className="aboutText">UCF Coding Bootcamp</h1>
+            <p>Recently graduated from the University of Central Florida's Coding Bootcamp.</p>
+            <p></p>
+          </div>
+          <img className="me" src="/code.jpg" alt="" />
         </div>
         <div className="aboutPage">
-          <img className="me" src="/me.jpeg" alt="" />
-          <p className="aboutText">hey</p>
+          <img className="me" src="/" alt="" />
+          <div>
+            <h1 className="aboutText">Bachelors in Health Sciences (Pre-Clinical)</h1>
+            <p>Recently graduated from the University of Central Florida's Coding Bootcamp.</p>
+            <p></p>
+          </div>
         </div>
       </div>
     );
