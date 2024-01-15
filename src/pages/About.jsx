bec3f872d0cbe335cc7073f7298d659f8422b81f
Travis <travis3214@yahoo.com>
2024-01-14 23:52:27 -0500

    added picture of paolo to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,16 +6,17 @@ export default function About() {
             <h1 className="aboutText">UCF Coding Bootcamp</h1>
             <p>I learned to code by going to the University of Central Florida's Coding Bootcamp.</p>
             <p>The course focused on Javascript and many of the framworks and libriaries that are widely used today.</p>
-            <p>Throughout the course I worked on many front-end and back-end projects.</p>
-            <p></p>
+            <p>Throughout the course I worked individually on many front-end and back-end projects.</p>
+            <p>I also got to work in groups on full-stack projects that we showed off to the class.</p>
           </div>
-          <img className="me" src="/code.jpg" alt="" />
+          <img className="me1" src="/code.jpg" alt="a picture of a computer with coding on screen" />
         </div>
         <div className="aboutPage">
-          <img className="me" src="/" alt="" />
+          <img className="me2" src="/paolo.jpeg" alt="a photo of an orlando magic basketball player" />
           <div>
-            <h1 className="aboutText">Active</h1>
-            <p></p>
+            <h1 className="aboutText">Remain Active</h1>
+            <p>When I'm not coding im usually hanging out with family, lifting weights, or playing basketball to maintain a healthy work life balance.</p>
+            <p>I'm a die hard Orlando Magic fan that loves supporting my home team!</p>
             <p></p>
           </div>
         </div>
