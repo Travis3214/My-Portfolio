1ea1874380209379a0bf7bbfe15f4832730a542e
Travis <travis3214@yahoo.com>
2024-01-29 18:49:37 -0500

    redoing about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,45 @@
 export default function About() {
     return (
-      <div className="title">
-        <div className="aboutPage1">
+      <div className="title aboutme">
+        <div className="bootcamp">
           <div>
-            <h1 className="aboutText">UCF Coding Bootcamp</h1>
-            <p>I learned to code by going to the University of Central Florida's Coding Bootcamp.</p>
-            <p>The course focused on Javascript and many of the framworks and libriaries that are widely used today.</p>
-            <p>Throughout the course I worked individually on many front-end and back-end projects.</p>
-            <p>I also got to work in groups on full-stack projects that we showed off to the class.</p>
+            <h1 className="aboutText">Coding Bootcamp</h1>
+            <p>
+              I learned to code by going to the University of Central Florida's Coding Bootcamp. The course focused on Javascript and many of the framworks and libriaries that are widely used today. Throughout the course I worked individually on many front-end and back-end projects. I also got to work in groups on full-stack projects that we showed off to the class.
+            </p>
           </div>
           <img className="me1" src="/code.jpg" alt="a picture of a computer with coding on screen" />
         </div>
-        <div className="aboutPage2">
+        <div className="stayactive">
           <img className="me2" src="/paolo.jpeg" alt="a photo of an orlando magic basketball player" />
           <div>
-            <h1 className="aboutText">Remain Active</h1>
-            <p>I'm a die hard Orlando Magic fan that loves supporting my home team!</p>
-            <p>When I'm not coding im usually hanging out with family, lifting weights, or playing basketball to maintain a healthy work life balance.</p>
-            <p></p>
+            <h1 className="aboutText">Stay Active</h1>
+            <p>
+              I'm a die hard Orlando Magic fan that loves supporting my home team! When I'm not coding im usually hanging out with family, lifting weights, or playing basketball to maintain a healthy work life balance.
+            </p>
           </div>
         </div>
       </div>
+        // <div className="title">
+      //   <div className="aboutPage1">
+      //     <div>
+      //       <h1 className="aboutText">UCF Coding Bootcamp</h1>
+      //       <p>
+      //         I learned to code by going to the University of Central Florida's Coding Bootcamp. The course focused on Javascript and many of the framworks and libriaries that are widely used today. Throughout the course I worked individually on many front-end and back-end projects. I also got to work in groups on full-stack projects that we showed off to the class.
+      //       </p>
+      //     </div>
+      //     <img className="me1" src="/code.jpg" alt="a picture of a computer with coding on screen" />
+      //   </div>
+      //   <div className="aboutPage2">
+      //     <img className="me2" src="/paolo.jpeg" alt="a photo of an orlando magic basketball player" />
+      //     <div>
+      //       <h1 className="aboutText">Remain Active</h1>
+      //       <p>
+      //         I'm a die hard Orlando Magic fan that loves supporting my home team! When I'm not coding im usually hanging out with family, lifting weights, or playing basketball to maintain a healthy work life balance.
+      //       </p>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
   
