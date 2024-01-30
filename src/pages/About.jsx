e2c8740c84dcd9c58bc48aed896f3d5cba62031f
Travis <travis3214@yahoo.com>
2024-01-30 16:48:24 -0500

    finished about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
           <div className="bootcampText">
             <h1 className="aboutText">Coding Bootcamp</h1>
             <p>
-              I learned to code by going to the University of Central Florida's Coding Bootcamp. The course focused on Javascript and many of the framworks and libriaries that are widely used today. Throughout the course I worked individually on many front-end and back-end projects. I also got to work in groups on full-stack projects that we showed off to the class.
+              I acquired my coding skills through the University of Central Florida's Coding Bootcamp. The program centered on comprehensive instruction in Javascript, encompassing various frameworks and libraries that are prevalent in recent development practices. Over the course, I undertook numerous front-end and back-end projects, showcasing my ability to handle diverse aspects of development. Additionally, I collaborated within group settings to build and deploy full-stack projects. These experiences have honed my proficiency in both individual and collaborative coding environments.
             </p>
           </div>
           <img className="me1" src="/code.jpg" alt="a picture of a computer with coding on screen" />
@@ -15,7 +15,7 @@ export default function About() {
           <div className="stayActiveText">
             <h1 className="aboutText">Stay Active</h1>
             <p>
-              I'm a die hard Orlando Magic fan that loves supporting my home team! When I'm not coding im usually hanging out with family, lifting weights, or playing basketball to maintain a healthy work life balance.
+              Outside of my professional pursuits in coding, I prioritize a well-rounded work-life balance. I invest my time in quality moments with family, engage in strength training for personal well-being, and actively participate in basketball, not only as a form of recreation but also as a commitment to maintaining a healthy lifestyle. These pursuits reflect my disciplined approach to both personal and professional aspects of life.
             </p>
           </div>
         </div>
